efeabaa4991ee2eabff668b0b988b35605db3fe2
abhishek29072002 <77395788+abhishek29072002@users.noreply.github.com>
2021-11-02 16:20:43 +0530

    Fourth Commit

--- src/Banner.js
+++ src/Banner.js
@@ -49,8 +49,6 @@ fetchData();
     },[])
 
 
-console.log(movie);
-
 
     function truncate(string,n) {
         return string?.length>n ? string.substr(0,n-1)+'...':string;
@@ -61,7 +59,7 @@ console.log(movie);
         style={{
             backgroundSize:'cover',
             backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-            backgroundPosition: "center center",   
+               
      }}> 
             <div className="banner-contents">
                 <h1 className="banner-title">
